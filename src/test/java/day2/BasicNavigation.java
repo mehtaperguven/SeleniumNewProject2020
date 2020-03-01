1bcd11f465217feea6eef2460e9cf7233e7c205d
mehtaperguven <mehtaperguven13@gmail.com>
2020-03-01 14:47:49 -0600

    First code commitment

--- src/test/java/day2/BasicNavigation.java
+++ src/test/java/day2/BasicNavigation.java
@@ -19,21 +19,53 @@ public class BasicNavigation {
         //in selenium, everything starts from WebDriver interface
 
         driver.get("http://google.com");// we opened the browser
-
-        Thread.sleep(3000);//it waits for 3 seconds
+        driver.manage().window().maximize();//It maximizes the browser's page
+        Thread.sleep(3000);//it waits for 3 seconds for Demo
         // we must add throws when we call sleep() from Thread
 
 
         String title=driver.getTitle();
         String expectedTitle="Google";
         System.out.println("Title from the HTML file is "+ title);
-        System.out.println("Hello hello");
+
         if (expectedTitle.equals(title)){
             System.out.println("TEST PASSED");
         }else{
             System.out.println("TEST FAILED");
         }
+driver.navigate().to("http://amazon.com");
+        Thread.sleep(3000);
 
-        driver.close();//we closed the browser
+        if (driver.getTitle().toLowerCase().contains("amazon")){
+            System.out.println("TEST PASSED");
+        }else{
+            System.out.println("TEST FAILED");
+        }
+
+
+        driver.navigate().back();//comes back to google
+
+        verifyEquals(driver.getTitle(),"Google");
+
+        driver.navigate().forward();//moves forward in the browser's history
+        System.out.println("Get title "+driver.getTitle());
+        System.out.println("Get URL "+driver.getCurrentUrl());
+
+
+        driver.navigate().refresh();//to refresh, reload the page
+        Thread.sleep(3000);
+
+        //driver.close() must be at the end
+        driver.close();//we close the browser
+
+
+    }public static void verifyEquals(String arg1, String arg2){
+
+        if (arg1.equals(arg2)){
+            System.out.println("TEST PASSED");
+        }else{
+            System.out.println("TEST FAILED");
+        }
     }
+
 }
