e13719872433dbfdb34d8a9dd47f16a15452a866
mehtaperguven <mehtaperguven13@gmail.com>
2020-03-01 13:05:56 -0600

    First code commitment

---
+++ src/test/java/day2/BasicNavigation.java
@@ -0,0 +1,39 @@
+package day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class BasicNavigation {
+    public static void main(String[] args) throws Exception {
+
+        //to start selenium script we need
+        //setup webdriver (browse driver) and
+        WebDriverManager.chromedriver().setup();
+        //RemoteWebDRiver is the super parent of all drivers
+       // RemoteWebDriver driver=new SafariDriver();
+
+        //ChromeDriver driver=new ChromeDriver();
+        WebDriver driver=new ChromeDriver();
+        //in selenium, everything starts from WebDriver interface
+
+        driver.get("http://google.com");// we opened the browser
+
+        Thread.sleep(3000);//it waits for 3 seconds
+        // we must add throws when we call sleep() from Thread
+
+
+        String title=driver.getTitle();
+        String expectedTitle="Google";
+        System.out.println("Title from the HTML file is "+ title);
+
+        if (expectedTitle.equals(title)){
+            System.out.println("TEST PASSED");
+        }else{
+            System.out.println("TEST FAILED");
+        }
+
+        driver.close();//we closed the browser
+    }
+}
