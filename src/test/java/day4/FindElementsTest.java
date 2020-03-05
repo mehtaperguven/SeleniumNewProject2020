02b38cabb755ce054be1bd0e0fad491fa900cc89
mehtaperguven <mehtaperguven13@gmail.com>
2020-03-04 20:34:03 -0600

    FindElements for list

--- src/test/java/day4/FindElementsTest.java
+++ src/test/java/day4/FindElementsTest.java
@@ -28,8 +28,31 @@ public class FindElementsTest {
         for (WebElement each:links){
 
             System.out.println(each.getText());
-        }
+            System.out.println(each.getAttribute("href"));
+            System.out.println();
+//            each.click();
+//            Thread.sleep(3000);
+//            driver.navigate().back();
+            //when we add above part it gives StaleElementReferenceException
 
-        driver.quit();
+        }
+        //Interview Question
+//StaleElementReferenceException: Means that selenium can not find previously located element
+        //we should find element again
+ //We need to refresh our collections each time to solve this problem       driver.quit();
+//you lost the ball you can not use it first you should find it
+
+    for (int i=1; i<links.size();i++){
+        links.get(i).click();//click to link
+        Thread.sleep(500);
+        driver.navigate().back();//go back
+        Thread.sleep(500);
+
+        links=driver.findElements(By.tagName("a"));//refresh all list of links again
+        //we lost the link in each implementation we should find all links again
+        //if we do not use ???
+
+    }
+
     }
 }
